093200b57919ec8448e6e58e59ba695da130057e
Patrick Coffey <patrickcoffey48@gmail.com>
2016-06-03 20:28:20 -0500

    Add a basic, empty cli

--- bin/bauen.js
+++ bin/bauen.js
@@ -5,3 +5,30 @@
  * Defines Bauen command line interface.
  */
 
+// Load dependencies.
+const BauenAPI = require('../lib/bauen.api');
+const Console = require('../lib/console.api');
+
+// Scaffold out Bauen object.
+BauenAPI.prototype.console = new Console();
+
+// Create a new instance of the CLI.
+const Bauen = new BauenAPI();
+
+// Initialize the command line interface properties.
+Bauen.initializeCli()
+
+// Initialize the current bauen project.
+.then(() => {
+  Bauen.initializeProject();
+})
+
+// Parse command line input and execute.
+.then(() => {
+  Bauen.execute();
+})
+
+// Catch any errors and surface them to the user.
+.catch((reason) => {
+  Bauen.console.log(reason, 'error');
+});
